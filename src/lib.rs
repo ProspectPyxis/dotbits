3b56b003d542f5229e32ab8b36df4e58c0a38815
ProspectPyxis <prospectpyxis@gmail.com>
2021-11-06 13:32:55 +0700

    feat: BitVec trait and impl

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,9 @@
 /// A trait that provides methods for simple bit manipulation.
 pub trait BitManip {
     /// Converts the implementor type into a `Vec<bool>`.
+    ///
+    /// The resulting vector is formatted in little-endian. To get the vector in big-endian,
+    /// reverse the array.
     fn bits(&self) -> Vec<bool>;
 
     /// Gets the bit at a specific position.
@@ -20,6 +23,35 @@ pub trait BitManip {
     ///
     /// This method will panic if the index is out of bounds, e.g. `pos >= Self::bit_len()`.
     fn bit_set(&mut self, pos: usize, val: bool) -> &mut Self;
+
+    /// Equivalent to `bit_set(&mut self, pos: usize, true)`.
+    ///
+    /// # Panics
+    ///
+    /// This method will panic if the index is out of bounds, e.g. `pos >= Self::bit_len()`.
+    #[inline]
+    fn bit_on(&mut self, pos: usize) -> &mut Self {
+        self.bit_set(pos, true)
+    }
+
+    /// Equivalent to `bit_set(&mut self, pos: usize, false)`.
+    ///
+    /// # Panics
+    ///
+    /// This method will panic if the index is out of bounds, e.g. `pos >= Self::bit_len()`.
+    #[inline]
+    fn bit_off(&mut self, pos: usize) -> &mut Self {
+        self.bit_set(pos, false)
+    }
+}
+
+/// A trait that provides additional helper methods on `Vec<bool>`.
+pub trait BitVec {
+    /// Returns a vector of all positions that are `true`.
+    fn ones(&self) -> Vec<usize>;
+
+    /// Returns a vector of all positions that are `false`.
+    fn zeroes(&self) -> Vec<usize>;
 }
 
 macro_rules! bitmanip_impl {
@@ -56,3 +88,31 @@ macro_rules! bitmanip_impl {
 }
 
 bitmanip_impl!(u8, u16, u32, u64, u128, usize);
+
+impl BitVec for Vec<bool> {
+    #[inline]
+    fn ones(&self) -> Vec<usize> {
+        let mut v: Vec<usize> = Vec::new();
+
+        for (i, b) in self.iter().enumerate() {
+            if *b {
+                v.push(i);
+            }
+        }
+
+        v
+    }
+
+    #[inline]
+    fn zeroes(&self) -> Vec<usize> {
+        let mut v: Vec<usize> = Vec::new();
+
+        for (i, b) in self.iter().enumerate() {
+            if !*b {
+                v.push(i);
+            }
+        }
+
+        v
+    }
+}
