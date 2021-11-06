93dd4db6f9fd0e1d8def2d69e174c3e02aacdc08
ProspectPyxis <prospectpyxis@gmail.com>
2021-11-06 15:08:25 +0700

    refactor: move BitManip trait to own file

--- src/lib.rs
+++ src/lib.rs
@@ -2,51 +2,9 @@
 
 #![warn(missing_docs)]
 
-/// A trait that provides methods for simple bit manipulation.
-pub trait BitManip {
-    /// Converts the implementor type into a `Vec<bool>`.
-    ///
-    /// The resulting vector is formatted in little-endian. To get the vector in big-endian,
-    /// `reverse()` the vector.
-    fn bits(&self) -> Vec<bool>;
+mod bit_manip;
 
-    /// Gets the length of the implementor type in bits.
-    fn bit_len() -> usize;
-
-    /// Gets the bit at a specific position.
-    ///
-    /// # Panics
-    ///
-    /// This method will panic if the index is out of bounds, e.g. `pos >= Self::bit_len()`.
-    fn bit_get(&self, pos: usize) -> bool;
-
-    /// Sets the bit at a specific position.
-    ///
-    /// # Panics
-    ///
-    /// This method will panic if the index is out of bounds, e.g. `pos >= Self::bit_len()`.
-    fn bit_set(&mut self, pos: usize, val: bool) -> &mut Self;
-
-    /// Equivalent to `bit_set(&mut self, pos: usize, true)`.
-    ///
-    /// # Panics
-    ///
-    /// This method will panic if the index is out of bounds, e.g. `pos >= Self::bit_len()`.
-    #[inline]
-    fn bit_on(&mut self, pos: usize) -> &mut Self {
-        self.bit_set(pos, true)
-    }
-
-    /// Equivalent to `bit_set(&mut self, pos: usize, false)`.
-    ///
-    /// # Panics
-    ///
-    /// This method will panic if the index is out of bounds, e.g. `pos >= Self::bit_len()`.
-    #[inline]
-    fn bit_off(&mut self, pos: usize) -> &mut Self {
-        self.bit_set(pos, false)
-    }
-}
+pub use crate::bit_manip::BitManip;
 
 /// A trait that provides additional helper methods on `Vec<bool>`.
 pub trait BitVec {
@@ -136,43 +94,3 @@ impl BitVec for Vec<bool> {
         try_into_usize = usize
     );
 }
-
-macro_rules! bitmanip_impl {
-    ($($t:ty),*) => {$(
-        impl BitManip for $t {
-            #[inline]
-            fn bits(&self) -> Vec<bool> {
-                let mut v: Vec<bool> = Vec::new();
-
-                for i in 0..Self::bit_len() {
-                    v.push(self.bit_get(i));
-                }
-
-                v
-            }
-
-            #[inline]
-            fn bit_len() -> usize {
-                Self::BITS as usize
-            }
-
-            #[inline]
-            fn bit_get(&self, pos: usize) -> bool {
-                assert!(pos < Self::bit_len());
-
-                *self & (1 << pos) != 0
-            }
-
-            #[inline]
-            fn bit_set(&mut self, pos: usize, val: bool) -> &mut Self {
-                assert!(pos < Self::bit_len());
-
-                *self ^= (Self::MIN.wrapping_sub(val.into()) ^ *self) & (1 << pos);
-
-                self
-            }
-        }
-    )*}
-}
-
-bitmanip_impl!(u8, u16, u32, u64, u128, usize);
