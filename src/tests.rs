00700e36f920d6364114b5e8d3fe28041ed30c88
ProspectPyxis <prospectpyxis@gmail.com>
2021-11-07 19:18:59 +0700

    test: more BitVec tests

--- src/tests.rs
+++ src/tests.rs
@@ -28,6 +28,19 @@ fn bit_set_err() {
 }
 
 #[test]
+fn bit_vec() {
+    assert_eq!(
+        0b01010101u8.bits(),
+        vec![true, false, true, false, true, false, true, false]
+    );
+    assert_eq!(
+        0b00011001u8.bits(),
+        vec![true, false, false, true, true, false, false, false]
+    );
+    assert_eq!(0u32.bits().len(), 32);
+}
+
+#[test]
 fn bit_vec_ones() {
     assert_eq!(0b00111000u8.bits().ones(), vec![3, 4, 5]);
     assert_eq!(0b10000000u8.bits().ones(), vec![7]);
@@ -39,6 +52,23 @@ fn bit_vec_zeroes() {
     assert_eq!(0b11001100u8.bits().zeroes(), vec![0, 1, 4, 5]);
     assert_eq!(0b01001111u8.bits().zeroes(), vec![4, 5, 7]);
     assert_eq!(0b00001111u8.bits().zeroes(), vec![4, 5, 6, 7]);
+}
+
+#[test]
+fn bit_vec_set() {
+    assert_eq!(
+        *0b01010101u8.bits().set(3, true).unwrap(),
+        0b01011101u8.bits()
+    );
+    assert_eq!(
+        *0b11001100u8.bits().set(2, false).unwrap(),
+        0b11001000u8.bits()
+    );
+    assert_eq!(*0b00001111u8.bits().set_on(4).unwrap(), 0b00011111u8.bits());
+    assert_eq!(
+        *0b00001111u8.bits().set_off(3).unwrap(),
+        0b00000111u8.bits()
+    );
 }
 
 #[test]
