d3c77bbc79841dca4f6556c7e1a87eb1afa254af
ProspectPyxis <prospectpyxis@gmail.com>
2021-11-07 18:05:36 +0700

    feat: bit_set returns Result

--- src/bit_manip.rs
+++ src/bit_manip.rs
@@ -1,52 +1,5 @@
 use crate::Error;
 
-/// A trait that provides methods for simple bit manipulation.
-pub trait BitManip {
-    /// Converts the implementor type into a `Vec<bool>`.
-    ///
-    /// The resulting vector is formatted in little-endian. To get the vector in big-endian,
-    /// `reverse()` the vector.
-    fn bits(&self) -> Vec<bool>;
-
-    /// Gets the length of the implementor type in bits.
-    fn bit_len() -> usize;
-
-    /// Gets the bit at a specific position.
-    ///
-    /// # Errors
-    ///
-    /// Will return `Err(Error::PosOutOfBounds)` if the index is out of bounds, e.g. `pos >=
-    /// Self::bit_len()`.
-    fn bit_get(&self, pos: usize) -> Result<bool, Error>;
-
-    /// Sets the bit at a specific position.
-    ///
-    /// # Panics
-    ///
-    /// This method will panic if the index is out of bounds, e.g. `pos >= Self::bit_len()`.
-    fn bit_set(&mut self, pos: usize, val: bool) -> &mut Self;
-
-    /// Equivalent to `bit_set(&mut self, pos: usize, true)`.
-    ///
-    /// # Panics
-    ///
-    /// This method will panic if the index is out of bounds, e.g. `pos >= Self::bit_len()`.
-    #[inline]
-    fn bit_on(&mut self, pos: usize) -> &mut Self {
-        self.bit_set(pos, true)
-    }
-
-    /// Equivalent to `bit_set(&mut self, pos: usize, false)`.
-    ///
-    /// # Panics
-    ///
-    /// This method will panic if the index is out of bounds, e.g. `pos >= Self::bit_len()`.
-    #[inline]
-    fn bit_off(&mut self, pos: usize) -> &mut Self {
-        self.bit_set(pos, false)
-    }
-}
-
 macro_rules! bitmanip_impl {
     ($($t:ty),*) => {$(
         impl BitManip for $t {
@@ -76,15 +29,64 @@ macro_rules! bitmanip_impl {
             }
 
             #[inline]
-            fn bit_set(&mut self, pos: usize, val: bool) -> &mut Self {
-                assert!(pos < Self::bit_len());
+            fn bit_set(&mut self, pos: usize, val: bool) -> Result<&mut Self, Error> {
+                if pos >= Self::bit_len() {
+                    return Err(Error::PosOutOfBounds);
+                }
 
                 *self ^= (Self::MIN.wrapping_sub(val.into()) ^ *self) & (1 << pos);
 
-                self
+                Ok(self)
             }
         }
     )*}
+}
+
+/// A trait that provides methods for simple bit manipulation.
+pub trait BitManip {
+    /// Converts the implementor type into a `Vec<bool>`.
+    ///
+    /// The resulting vector is formatted in little-endian. To get the vector in big-endian,
+    /// `reverse()` the vector.
+    fn bits(&self) -> Vec<bool>;
+
+    /// Gets the length of the implementor type in bits.
+    fn bit_len() -> usize;
+
+    /// Gets the bit at a specific position.
+    ///
+    /// # Errors
+    ///
+    /// Will return `Err(Error::PosOutOfBounds)` if the index is out of bounds, e.g. `pos >=
+    /// Self::bit_len()`.
+    fn bit_get(&self, pos: usize) -> Result<bool, Error>;
+
+    /// Sets the bit at a specific position.
+    ///
+    /// # Panics
+    ///
+    /// This method will panic if the index is out of bounds, e.g. `pos >= Self::bit_len()`.
+    fn bit_set(&mut self, pos: usize, val: bool) -> Result<&mut Self, Error>;
+
+    /// Equivalent to `bit_set(&mut self, pos: usize, true)`.
+    ///
+    /// # Panics
+    ///
+    /// This method will panic if the index is out of bounds, e.g. `pos >= Self::bit_len()`.
+    #[inline]
+    fn bit_on(&mut self, pos: usize) -> Result<&mut Self, Error> {
+        self.bit_set(pos, true)
+    }
+
+    /// Equivalent to `bit_set(&mut self, pos: usize, false)`.
+    ///
+    /// # Panics
+    ///
+    /// This method will panic if the index is out of bounds, e.g. `pos >= Self::bit_len()`.
+    #[inline]
+    fn bit_off(&mut self, pos: usize) -> Result<&mut Self, Error> {
+        self.bit_set(pos, false)
+    }
 }
 
 bitmanip_impl!(u8, u16, u32, u64, u128, usize);
